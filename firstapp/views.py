a0162e7f762bb05630710f7a289300d7d2f277c4
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-12 12:21:18 +0300

    Work with bd via page

--- firstapp/views.py
+++ firstapp/views.py
@@ -20,14 +20,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
 
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
+from .models import Persons
 
+
+# получение данных из бд
 def index(request):
+    people = Persons.objects.all()
+    return render(request, "index.html", {"people": people})
+
+
+# сохранение данных в бд
+def create(request):
     if request.method == "POST":
-        name = request.POST.get("name")
-        age = request.POST.get("age")     # получение значения поля age
-        comment = request.POST.get("comment")
-        email = request.POST.get("email")
-        return HttpResponse("<h2>Hello, {0}! age: {1}, email: {2}, comment: {3}</h2>".format(name,age,email,comment))
-    else:
-        userform = UserForm()
-        return render(request, "index.html", {"form": userform})
+        person = Persons()
+        person.name = request.POST.get("name")
+        person.addtime = request.POST.get("addtime")
+        person.save()
+    return HttpResponseRedirect("/")
+
+
+def edit(request, id):
+    try:
+        person = Persons.objects.get(id=id)
+
+        if request.method == "POST":
+            person.name = request.POST.get("name")
+            person.addtime = request.POST.get("addtime")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"person": person})
+    except Persons.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
+
+
+# удаление данных из бд
+def delete(request, id):
+    try:
+        person = Persons.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect("/")
+    except Persons.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
--- test_db/urls.py
+++ test_db/urls.py
@@ -28,6 +28,12 @@ from firstapp import views
 # ]
 
 
+from django.urls import path
+from firstapp import views
+
 urlpatterns = [
     path('', views.index),
+    path('create/', views.create),
+    path('edit/<int:id>/', views.edit),
+    path('delete/<int:id>/', views.delete),
 ]
